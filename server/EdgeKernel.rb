14a87c2fa4e6f3f1ae68810ca5fbb8c3c0273f85
Chetan Mistry <cmistry98@gmail.com>
2020-06-12 13:39:55 +0100

    Rubocop EdgeKernel File
    Signed-off-by: Chetan Mistry <cmistry98@gmail.com>

--- server/EdgeKernel.rb
+++ server/EdgeKernel.rb
@@ -1,4 +1,3 @@
-# coding: utf-8
 require 'rubygems'
 require 'redis'
 require 'json'
@@ -9,39 +8,34 @@ require 'usagewatch_ext'
 require 'timeout'
 require 'csv'
 require 'socket'
-def check_webdis()
-  ip = Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address
-  uri = "http://" + ip.to_s + ":7379/GET/hello"
+def check_webdis
+  # ip = Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address
+  uri = 'http://' + ip.to_s + ':7379/GET/hello'
   uri = URI(uri)
   response = Net::HTTP.get_response(uri)
-  if response.code != '200' then
-    fork{
-      exec("echo error;cd ../webdis; ./webdis")
-    }
+  if response.code != '200'
+    fork { exec('echo error;cd ../webdis; ./webdis') }
     return false
   end
-  return true
+  true
 end
 
-def load_kernels_to_memory()
-  fork{
-    exec("vmtouch -tq ../Kernels/")
-  }
+def load_kernels_to_memory
+  fork { exec('vmtouch -tq ../Kernels/') }
 end
-
 
 def fill_queues(queue, name)
-      for q in queue do
-        if $queues[q].length == 0 then
-          $queues[q] = [name]
-        else
-          $queues[q].append(name)
-        end
-      end
+  queue.each do |q|
+    if $queues[q].empty?
+      $queues[q] = [name]
+    else
+      $queues[q].append(name)
+    end
+  end
 end
 
-
-# Map between queues and which unikernels to boot when data is recieved on that queue
+# Map between queues and which unikernels to boot when data is recieved on that
+# queue
 $queues = Hash.new([])
 
 # Map between the unikernel to run and where to find it
@@ -56,92 +50,131 @@ $kernel_limit = Hash.new(1)
 # Map between the unikernel and the number of instances currently running
 $active_kernels = Hash.new(0)
 
-# Map between the unikernel and whether or not it has any output files which need aggregating
+# Map between the unikernel and whether or not it has any output files which
+# need aggregating
 $has_output = Hash.new(false)
 
-#Map between the unikernel and the how large the queue needs to be before a new instance can spawn
+# Map between the unikernel and the how large the queue needs to be before a new
+# instance can spawn
 $scale_thresholds = Hash.new(10)
 
-#Map between the unikernel and the time it needs to wait in seconds between each new instance is spawned
+# Map between the unikernel and the time it needs to wait in seconds between
+# each new instance is spawned
 $grace_period = Hash.new(0)
 
-#Map bewtween the unikernel and the platform which it runs on, default is mirageos unikernels on solo5
-$binary_type = Hash.new("mirageos")
+# Map bewtween the unikernel and the platform which it runs on, default is
+# MirageOS unikernels on Solo5
 
+$binary_type = Hash.new('mirageos')
 
-#List of tap devices available 
+# List of tap devices available
 $tap_interfaces = []
 
-$base_dir =  Dir.pwd
+$base_dir = Dir.pwd
 
 $mutex = Mutex.new
 
 $usw = Usagewatch
 
-def register_unikernels()
-  Dir.chdir('config')
-  files = Dir.glob("*.json")
-  for f in files do
-    json = JSON.parse(File.read(f))
-    name = json["name"]
-    dir = json["directory"]
-    queue = json["queue"]
-    ofile = json["ofile"]
-    limit = json["max_instances"]
-    threshold = json["scale_threshold"]
-    binary_type = json["binary"]
-    fill_queues(queue, name)
-    
-    $directories[name] = (__dir__ + '/../' + dir).to_s
-    
-    if limit then
-      $kernel_limit[name] = limit
-    end
-    if threshold then
+def get_metadata(f)
+  json = JSON.parse(File.read(f))
+  name = json['name']
+  dir = json['directory']
+  queue = json['queue']
+  ofile = json['ofile']
+  limit = json['max_instances']
+  threshold = json['scale_threshold']
+  binary_type = json['binary']
+  [name, dir, queue, ofile, limit, threshold, binary_type]
+end
+
+def assign_directory(dir, name)
+  $directories[name] = (__dir__ + '/../' + dir).to_s
+end
+
+def define_limit(limit, name)
+  if limit
+    $kernel_limit[name] = limit
+  end
+end
+
+def define_scale_threshold(threshold, name)
+  if threshold
       $scale_thresholds[name] = threshold
-    end
-    if binary_type then
+  end
+end
+
+def specify_binary(binary_type, name)
+  if binary_type
       $binary_type[name] = binary_type
-    end
-    $ofiles[name] = ofile
   end
 end
 
-$redis = Redis.new()
+def assign_ofile(ofile, name)
+  $ofiles[name] = ofile
+end
 
-def establish_channels()
+def create_mappings(name, dir, queue, ofile, limit, threshold, binary_type)
+  fill_queues(queue, name)
+  assign_directory(dir, name)
+  define_limit(limit, name)
+  define_scale_threshold(threshold, name)
+  specify_binary(binary_type, name)
+  assign_ofile(ofile, name)
+end
+
+def register_unikernels
+  Dir.chdir('config')
+  files = Dir.glob('*.json')
+  files.each do |f|
+    name, dir, queue, ofile, limit, threshold, binary_type = get_metadata(f)
+
+    create_mappings(name, dir, queue, ofile, limit, threshold, binary_type)
+  end
+end
+
+$redis = Redis.new
+
+def establish_channels
   channels = []
-  $queues.each do | key, value |
-    null_process_queue = key + "_null_process_queue"
-    channels.append([Redis::Queue.new(key, null_process_queue, :redis=> $redis), key])
+  $queues.each_key do |key|
+    null_process_queue = key + '_null_process_queue'
+    channels.append([Redis::Queue.new(key, null_process_queue, :redis => $redis),
+                     key])
   end
-  return channels
+  channels
 end
 
-
-def generate_tap_list()
+def generate_tap_list
   for i in 2..51 do
-    tapN = "tap1000".concat(i.to_s)
-    ipN = "10.0.0.".concat(i.to_s).concat("/24")
+    tapN = 'tap1000'.concat(i.to_s)
+    ipN = '10.0.0.'.concat(i.to_s).concat('/24')
     $tap_interfaces.append([tapN, ipN, true])
+  end
+end
+
+def generate_command(kernel, tap_index)
+  if $binary_type[kernel].eql? 'bash'
+    './' + kernel
+  else
+    'solo5-hvt --net:service=' + $tap_interfaces[tap_index][0] +
+      ' -- ' +
+      kernel +
+      ' --ipv4=' +
+      $tap_interfaces[tap_index][1] +
+      ' --ipv4-gateway=10.0.0.1'
   end
 end
 
 def execute_kernel(kernel, part_count, tap_index)
   Dir.chdir($directories[kernel])
-  if $binary_type[kernel].eql? "bash" then
-    command = "./"+ kernel
-  else
-    command = "solo5-hvt --net:service=" + $tap_interfaces[tap_index][0] + "  -- " + kernel + " --ipv4="+ $tap_interfaces[tap_index][1] + " --ipv4-gateway=10.0.0.1"
-  end
-  ofile = $base_dir + "/output/" + $ofiles[kernel] + ".part."+part_count
-
-  puts "Executing"
-  
-  PTY.spawn( command ) do |stdout, stdin, pid|
+  command = generate_command(kernel, tap_index)
+  ofile = $base_dir + '/output/' + $ofiles[kernel] + '.part.' + part_count
+  puts 'Executing'
+  PTY.spawn(command) do |stdout, _stdin, pid|
     begin
-      Timeout::timeout(1){
-        open(ofile, "a") do |f|
+      Timeout.timeout(1) {
+        open(ofile, 'a') do |f|
           stdout.each { |line| f.puts line }
         end
       }
@@ -150,33 +183,29 @@ def execute_kernel(kernel, part_count, tap_index)
       Process.kill(9, pid)
     end
   end
-
 end
 
-def get_tap_device()
-  for i in 0 .. $tap_interfaces.length-1 do
-    if $tap_interfaces[i][2] then
+def get_tap_device
+  for i in 0..($tap_interfaces.length - 1) do
+    if $tap_interfaces[i][2]
       return i
     end
   end
-  return -1
+  -1
 end
 
-def init()
-  active = check_webdis()
-  while !active do
-    active = check_webdis()
-  end
-  load_kernels_to_memory()
-  generate_tap_list()
-  register_unikernels()
- 
+def init
+  active = check_webdis
+  until active do active = check_webdis end
+  load_kernels_to_memory
+  generate_tap_list
+  register_unikernels
 end
 
 def combine_logs(kernel)
-  ofile = $base_dir + "/output/" + $ofiles[kernel]
-  open(ofile, 'a'){ |f|
-    Dir.glob($base_dir + "/output/" + $ofiles[kernel] + ".*").sort.each{ |file|
+  ofile = $base_dir + '/output/' + $ofiles[kernel]
+  open(ofile, 'a') { |f|
+    Dir.glob($base_dir + '/output/' + $ofiles[kernel] + '.*').sort.each { |file|
       f.puts File.readlines(file)
       File.delete(file)
     }
@@ -184,27 +213,26 @@ def combine_logs(kernel)
   $has_output[kernel] = false
 end
 
-#Function used to terminate EdgeKernel when a queue is empty and report
-def terminate_on(c, target,u,ts)
-if c[1] == target then
-          if c[0].empty? and $active_kernels[u] == 0 then
-            finished = (`date +%s%N`.to_i - ts)/1000000
-            puts finished
-            exit 0
-          end
-        end
+# Function used to terminate EdgeKernel when a queue is empty and report
+def terminate_on(c, target ,u, ts)
+  if c[1] == target
+    if c[0].empty? && $active_kernels[u].zero?
+      finished = (`date +%s%N`.to_i - ts) / 1_000_000
+      puts finished
+      exit 0
+    end
+  end
 end
 
-
-def server()
+def server
   start_mem = `ps -o rss= -p #{$$}`.to_i
   n_kernels = 0
   loop do
-      active = check_webdis()
-      while !active do
-        active = check_webdis()
-      end
-    channels = establish_channels()
+    active = check_webdis
+    until active do
+      active = check_webdis
+    end
+    channels = establish_channels
     for c in channels do
       queue_name = c[1]
       for u in $queues[queue_name] do
@@ -215,19 +243,19 @@ def server()
         else
           queue_count = c[0].length
           if $active_kernels[u] > queue_count/$scale_thresholds[u] then
-            puts "wait to finish before creating new"
+            puts 'wait to finish before creating new'
             next
           else
             if $active_kernels[u] >= $kernel_limit[u] then
-              "Limit Met, allow existing to terminate"
+              'Limit Met, allow existing to terminate'
               next
             elsif $grace_period[u] + 0 > Time.now.to_i then
-              puts "Still within grace-period, please wait"
+              puts 'Still within grace-period, please wait'
               next
             else
               tap_index = get_tap_device()
               if tap_index == -1 then
-                puts "No Tap Device available"
+                puts 'No Tap Device available'
                 next
               end
               $mutex.synchronize do
@@ -235,27 +263,25 @@ def server()
                 $grace_period[u] = Time.now.to_i
                 $tap_interfaces[tap_index][2] = false
 
-
               end
 
-              # When starting a new thread, the next iteration of the loop executes
-              # and so this line is needed to ensure the correct unikernel boots
+              # When starting a new thread, the next iteration of the loop
+              # executes and so this line is needed to ensure the correct
+              # unikernel boots
               kernel = u
               $has_output[kernel] = true
               n_kernels += 1
 
-              puts "SPAWNING " + kernel
-              Thread.new(kernel, tap_index) {|kernel, tp|
+              puts 'SPAWNING ' + kernel
+              Thread.new(kernel, tap_index) { |kernel, tp|
                 execute_kernel(kernel, $active_kernels[kernel].to_s, tp)
                 $mutex.synchronize do
-                  puts "Finished"
+                  puts 'Finished'
                   $active_kernels[kernel] -= 1
                   $grace_period[kernel] = 0
                   $tap_interfaces[tp][2] = true
-
                 end
               }
-              
             end
           end
         end
@@ -264,10 +290,9 @@ def server()
   end
 end
 
-def main()
-  init()
-  server()
-
+def main
+  init
+  server
 end
 
-main()
+main
